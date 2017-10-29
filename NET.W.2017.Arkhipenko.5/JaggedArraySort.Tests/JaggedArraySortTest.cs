1d8adba09cdaf2e35f659f60f82d8ce83e45fc44
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-10-29 16:41:17 +0300

    1

--- NET.W.2017.Arkhipenko.5/JaggedArraySort.Tests/JaggedArraySortTest.cs
+++ NET.W.2017.Arkhipenko.5/JaggedArraySort.Tests/JaggedArraySortTest.cs
@@ -17,6 +17,13 @@ namespace JaggedArraySort.Tests
             arrayTest[1] = new int[] {1, 2, 8, 12};
             arrayTest[2] = new int[] {5, 10, 16};
             arrayTest[3] = new int[] {18, 1, 5, 6, 8};
+           
+            CollectionAssert.AreEqual(JaggedArraySort.BubbleSortForJaggedArrayUpSum(arrayTest, colums), new int[]{23, 30, 31, 38 });
+            CollectionAssert.AreEqual(JaggedArraySort.BubbleSortForJaggedArrayDownSum(arrayTest, colums), new int[] { 38, 31, 30, 23 });
+            CollectionAssert.AreEqual(JaggedArraySort.BubbleSortForJaggedArrayUpMax(arrayTest, colums), new int[] { 12, 16, 18, 20});
+            CollectionAssert.AreEqual(JaggedArraySort.BubbleSortForJaggedArrayDownMax(arrayTest, colums), new int[] { 20, 18, 16, 12 });
+            CollectionAssert.AreEqual(JaggedArraySort.BubbleSortForJaggedArrayUpMin(arrayTest, colums), new int[] { 1, 1, 5, 10 });
+            CollectionAssert.AreEqual(JaggedArraySort.BubbleSortForJaggedArrayDownMin(arrayTest, colums), new int[] { 10, 5, 1, 1 });
 
         }
     }
---
+++ NET.W.2017.Arkhipenko.5/Polynomial/Polynomial.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Polynomial
+{
+    public class Polynomial
+    {
+        private readonly Double[] coefficients;
+        
+        public double[] Coefficients
+        {
+            get { return coefficients; }
+        }
+
+       
+
+        public Polynomial( double [] coefficients)
+        {
+            this.coefficients = coefficients;
+        }
+
+        public override string ToString()
+        {
+            string polynomialToString = "F(x) = ";
+            for (int i = Coefficients.Length-1; i > 0; i--)
+            {
+                polynomialToString += Coefficients[i] + "x^" + (i+1) + "+";
+            }
+
+            return polynomialToString + Coefficients[0] +"x^1";
+        }
+
+        public override bool Equals(object pol)
+        {
+            Polynomial coef = (Polynomial)pol;
+            
+            if  (coef == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (Coefficients.Length != coef.Coefficients.Length)
+            {
+                return false;
+            }
+
+            int k = 0;
+            for (int i = 0; i < coef.Coefficients.Length; i++)
+            {
+                if (Coefficients[i] == coef.Coefficients[i])
+                {
+                    k++;
+                }
+            }
+
+            if (k == coef.Coefficients.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Coefficients.Length;
+        }
+
+        public static bool operator ==(Polynomial pol1, Polynomial pol2)
+        {
+            
+            return pol1.Equals(pol2);
+        }
+
+        public static bool operator !=(Polynomial pol1, Polynomial pol2)
+        {
+            if (pol1.Equals(pol2))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static Polynomial operator +(Polynomial pol1, Polynomial pol2)
+        {
+            double[] sumCoef = new double[Math.Max(pol1.Coefficients.Length, pol2.Coefficients.Length)];
+
+            if (pol1.Coefficients.Length == pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol1.Coefficients.Length; i++)
+                {
+                    sumCoef[i] = pol1.Coefficients[i] + pol2.Coefficients[i];
+                }
+            }
+
+
+            if (pol1.Coefficients.Length < pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol1.Coefficients.Length; i++)
+                {
+                    sumCoef[i] = pol1.Coefficients[i] + pol2.Coefficients[i];
+                }
+                for (int i = pol1.Coefficients.Length; i < pol2.Coefficients.Length; i++)
+                {
+                    sumCoef[i] = pol2.Coefficients[i];
+                }
+            }
+
+            if (pol1.Coefficients.Length > pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol2.Coefficients.Length; i++)
+                {
+                    sumCoef[i] = pol1.Coefficients[i] + pol2.Coefficients[i];
+                }
+                for (int i = pol2.Coefficients.Length; i < pol1.Coefficients.Length; i++)
+                {
+                    sumCoef[i] = pol1.Coefficients[i];
+                }
+            }
+
+            Polynomial polSum = new Polynomial(sumCoef);
+            return polSum;
+        }
+
+        public static Polynomial operator -(Polynomial pol1, Polynomial pol2)
+        {
+            double[] subCoef = new double[Math.Max(pol1.Coefficients.Length, pol2.Coefficients.Length)];
+
+            if (pol1.Coefficients.Length == pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol1.Coefficients.Length; i++)
+                {
+                    subCoef[i] = pol1.Coefficients[i] - pol2.Coefficients[i];
+                }
+            }
+
+
+            if (pol1.Coefficients.Length < pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol1.Coefficients.Length; i++)
+                {
+                    subCoef[i] = pol1.Coefficients[i] - pol2.Coefficients[i];
+                }
+                for (int i = pol1.Coefficients.Length; i < pol2.Coefficients.Length; i++)
+                {
+                    subCoef[i] = pol2.Coefficients[i];
+                }
+            }
+
+            if (pol1.Coefficients.Length > pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol2.Coefficients.Length; i++)
+                {
+                    subCoef[i] = pol1.Coefficients[i] - pol2.Coefficients[i];
+                }
+                for (int i = pol2.Coefficients.Length; i < pol1.Coefficients.Length; i++)
+                {
+                    subCoef[i] = pol1.Coefficients[i];
+                }
+            }
+
+            Polynomial polSum = new Polynomial(subCoef);
+            return polSum;
+        }
+
+
+        public static Polynomial operator *(Polynomial pol1, Polynomial pol2)
+        {
+            double[] mulCoef = new double[Math.Max(pol1.Coefficients.Length, pol2.Coefficients.Length)];
+
+            if (pol1.Coefficients.Length == pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol1.Coefficients.Length; i++)
+                {
+                    mulCoef[i] = pol1.Coefficients[i] * pol2.Coefficients[i];
+                }
+            }
+
+
+            if (pol1.Coefficients.Length < pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol1.Coefficients.Length; i++)
+                {
+                    mulCoef[i] = pol1.Coefficients[i] * pol2.Coefficients[i];
+                }
+                for (int i = pol1.Coefficients.Length; i < pol2.Coefficients.Length; i++)
+                {
+                    mulCoef[i] = pol2.Coefficients[i];
+                }
+            }
+
+            if (pol1.Coefficients.Length > pol2.Coefficients.Length)
+            {
+                for (int i = 0; i < pol2.Coefficients.Length; i++)
+                {
+                    mulCoef[i] = pol1.Coefficients[i] * pol2.Coefficients[i];
+                }
+                for (int i = pol2.Coefficients.Length; i < pol1.Coefficients.Length; i++)
+                {
+                    mulCoef[i] = pol1.Coefficients[i];
+                }
+            }
+
+            Polynomial polSum = new Polynomial(mulCoef);
+            return polSum;
+        }
+    }
+}
